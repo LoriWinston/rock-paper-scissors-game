9fc5f1e2bfde08689e9c4d82145f18e6a40d8424
Lori Winston <Lcwinston@gmail.com>
2021-01-13 14:54:26 -0800

    first draft

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
 
+const winSpan = document.getElementById('wins');
+const lossSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
+
+
 // initialize state
+
+
+
+
+
 
 // set event listeners to update state and DOM
