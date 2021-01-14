b6a979c68bbaea823c616c0cccb79720622e5558
Lori Winston <Lcwinston@gmail.com>
2021-01-14 14:18:53 -0800

    updated version

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 import { randomThrow } from './utils.js'
 
+import { doesUserWin } from './utils.js'
+
 const winSpan = document.getElementById('wins');
 const lossSpan = document.getElementById('losses');
+const drawSpan = document.getElementById('draw');
 const totalSpan = document.getElementById('total');
+const results = document.getElementById('results');
 
+
+let wins = 0;
+let total = 0;
+let draw = 0;
+
+
+
+
+
+submitButton.addEventListener ('click', () => {
+
+
+
+
+
+
+
+
+})
 
 
 
@@ -15,4 +38,3 @@ const totalSpan = document.getElementById('total');
 
 
 // set event listeners to update state and DOM
-console.log(randomThrow(1))
--- utils.js
+++ utils.js
@@ -10,7 +10,11 @@ export function randomThrow(rockPaperScissors) {
 }
 
 
-
-
-
-
+export function doesUserWin(player, computer) {
+    if (player === computer) return 'draw'
+    if (player === 'rock' && computer === 'paper') return 'lose'
+    if (player === 'scissors' && computer === 'rock') return 'lose'
+    if (player === 'paper' && computer === 'scissors') return 'lose'
+    else return 'win' 
+}
+    
