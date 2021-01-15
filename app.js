339e24bb5e40e9489bf49aab2b3e4766e3b23d27
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-14 18:39:58 -0700

    pair editing

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-import { randomThrow } from './utils.js'
+import { getGameOutcome } from './utils.js'; 
+import { playerRules, playerOption } from './the-rules.js';
 
 const winSpan = document.getElementById('wins');
 const lossSpan = document.getElementById('losses');
 const totalSpan = document.getElementById('total');
+const tieSpan = document.getElementById('ties');
+const Button = document.getElementById('playButton');
+const secretMessage = document.getElementById('secretBox');
 
-
-
+// console.log(winSpan, lossSpan, totalSpan, Button, secretMessage, tieSpan);
 
 // initialize state
-
-
-
-
+let wins = 0; 
+let tie = 0; 
+let loss = 0; 
 
 
 // set event listeners to update state and DOM
-console.log(randomThrow(1))
+Button.addEventListener('click', () => {
+    // console.log('im in the button');
+    const computerThrow = Math.ceil(Math.random() * 2);
+    // console.log(computerThrow);
+    let computerPlayer = playerOption[computerThrow];
+    // console.log(computerPlayer);
+    const userPlayer = document.querySelector('input[type="radio"]:checked');
+    // console.log(userPlayer['value']);
+    const gameOutcome = playerRules[userPlayer.value][computerPlayer]; 
+    // console.log(gameOutcome);
+    [wins, tie, loss] = getGameOutcome(gameOutcome, secretMessage);
+
+    // console.log(playerRules[userPlayer.value]);
+
+    totalSpan.textContent = wins + loss + tie;
+    winSpan.textContent = wins; 
+    lossSpan.textContent = loss;
+    tieSpan.textContent = tie; 
+});
--- utils.js
+++ utils.js
@@ -1,16 +1,29 @@
-export function randomThrow(rockPaperScissors) {
-    if (rockPaperScissors === 1) {
-        return 'scissors';
-    } else if (rockPaperScissors === 0) {
-        return 'paper';
-    } else if(rockPaperScissors === -1) {
-        return 'rock';
+// export function computerThrow(rockPaperScissors) {
+//     if (rockPaperScissors === 1) {
+//         return 'scissors';
+//     } else if (rockPaperScissors === 0) {
+//         return 'paper';
+//     } else if (rockPaperScissors === 2) {
+//         return 'rock';
+//     }
+
+// }
+let Wins = 0;
+let Loss = 0; 
+let Tie = 0; 
+
+
+export function getGameOutcome(gameOutcome, secretMessage) {
+    if (gameOutcome === 'win') {
+        Wins++;
+        secretMessage.textContent = 'Great Job! You Win!';
+    } else if (gameOutcome === 'loss') {
+        Loss++;
+        secretMessage.textContent = 'Darn it, you lost!'; 
+    } else {
+        Tie++;
+        secretMessage.textContent = 'Its a tie!';
     }
-
+    return [Wins, Loss, Tie];
 }
-
-
-
-
-
 
